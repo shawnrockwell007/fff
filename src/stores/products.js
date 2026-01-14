30721658bff3f099ab5fc5a03e50363773386c23
shawnrockwell007 <shawnrockwell007@proton.me>
2026-01-14 11:47:56 +0100

    added new toya

--- src/stores/products.js
+++ src/stores/products.js
@@ -36,6 +36,8 @@ export const useProductStore = defineStore('product', {
       { id: 24, name: 'Bondage Tape', price: '7.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/59251_a71_red_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'toy', level: 'intermediate' },
       { id: 25, name: 'Rose Pump Clittoral Stimulation', price: '60.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/86861_a48906_red_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'toy', level: 'advanced' },
       { id: 26, name: 'Clittoris And Nipple Pump', price: '17.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/74217_a40672_clear_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'toy', level: 'beginner' },
+      { id: 42, name: 'Womanizer Blend Rechargeable G-Spot and Clitoral Stimulator', price: '129.00', image:'https://media.lovehoneyassets.com/i/lovehoney/86486_a48623_blue_000?$primary$&h=742&w=556&fmt=auto&qlt=80', type: 'toy', level: 'advanced' },
+
       { id: 27, name: 'Autosuction Nipple Pump', price: '60.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/81897_a45439_clear_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'toy', level: 'intermediate' },
       { id: 28, name: 'Pussy Pump And Tongue Vibrator Kit', price: '120.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/75341_a41359_purple_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'toy', level: 'intermediate' },
       { id: 29, name: 'Pennis Pump', price: '40.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/46571_a28656_clear_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'toy', level: 'advanced' },
@@ -45,13 +47,15 @@ export const useProductStore = defineStore('product', {
       { id: 33, name: 'Luxury Whip Combo', price: '40.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/86264_a48435_black_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'toy', level: 'advanced' },
       { id: 34, name: 'Sting Riding Crop', price: '20.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/40182_a26081_silver_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'toy', level: 'advanced' },
       { id: 35, name: 'Door Hnaging Sex Wing', price: '80.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/87588_a49565_black_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'toy', level: 'advanced' },
+      { id: 36, name: 'Sex Position Wedge Ramp Combo ', price: '249.00', image:'https://media.lovehoneyassets.com/i/lovehoney/20864_a8853_black_000?$primary$&h=742&w=556&fmt=auto&qlt=80', type: 'toy', level: 'advanced' },
 
+      
       // Wellness (no sizes)
-      { id: 36, name: 'Water Based Lube', price: '10.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/37109_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'wellness', level: 'advanced' },
-      { id: 37, name: 'Liquid Silk Lube', price: '12.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/7163_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'wellness', level: 'advanced' },
-      { id: 42, name: 'Anal Lube', price: '10.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/45600_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'wellness', level: 'advanced' },
-      { id: 43, name: 'Anti Bacterial Cleanser', price: '10.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/37110_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'wellness', level: 'advanced' },
-      { id: 44, name: 'Toy Battery', price: '3.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/82362_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'wellness', level: 'advanced' }
+      { id: 37, name: 'Water Based Lube', price: '10.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/37109_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'wellness', level: 'advanced' },
+      { id: 38, name: 'Liquid Silk Lube', price: '12.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/7163_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'wellness', level: 'advanced' },
+      { id: 39, name: 'Anal Lube', price: '10.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/45600_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'wellness', level: 'advanced' },
+      { id: 40, name: 'Anti Bacterial Cleanser', price: '10.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/37110_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'wellness', level: 'advanced' },
+      { id: 41, name: 'Toy Battery', price: '3.00', image: 'https://media.lovehoneyassets.com/i/lovehoney/82362_000?$primary$&h=371&w=278&fmt=auto&qlt=80', type: 'wellness', level: 'advanced' }
     ]
   }),
 
